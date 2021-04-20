a2e0c361cbf0c159bff34a93c9f8e0ba2d45c598
Jason Rabely <rabelyj@gmail.com>
2021-04-20 08:47:15 +0200

    Fist Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // states
+  // useEffect(loadPokemon, [])
+  const [pokemon, setPokemon] = useState({
+    name: '',
+    sprites: {
+
+    }
+  })
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const id = form.get('id');
+    loadPokemon(id);
+  }
+  function loadPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then((response) => {
+      return response.json()
+    })
+    .then((poke) => {
+      setPokemon(poke)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Nombre Pokemon: {pokemon.name}</p>
+      <img src={pokemon.sprites.front_default} alt=""/>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" name="id"/>
+      </form>
     </div>
   );
 }
